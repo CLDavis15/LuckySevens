3a58976d5aaf477f05ab2fc012afc4aa3f5da23c
Caleb Davis <calebldavis0@gmail.com>
2019-10-21 12:11:10 -0400

    add the final files to game.

---
+++ luckySevens.js
@@ -0,0 +1,81 @@
+
+  function play(bet) {
+      var money = bet;
+      var maxMoney = money;
+      var rolls = 0;
+      var rollAtMax = 0;
+      
+      
+      while (money > 0) {
+          
+          var diceRoll = rollDice();
+          
+          if (diceRoll == 7) {
+              money = money + 4;
+            } else {
+                money = money - 1;
+            }
+            
+            rolls++;
+            
+            if (money > maxMoney) {
+                maxMoney = money;
+                rollAtMax = rolls;
+            }
+            
+           
+        }
+        
+        document.getElementById('resultBet').innerText = bet;
+        document.getElementById('resultBeforeBroke').innerText = rolls;
+        document.getElementById('resultMaxMoney').innerText = maxMoney;
+        document.getElementById('resultRollAtMax').innerText = rollAtMax;
+        document.getElementById("results").style.display="block";
+        document.getElementById("play").innerText="Play Again";
+    }
+    
+    function rollDice() {
+        return (rollSixsided() + rollSixsided());
+    }
+    
+    function rollSixsided() {
+        var num = Math.floor(Math.random() * 6) + 1;
+        return num;
+    }
+    function verify() {
+        clearErrors();
+      
+        var bet = document.getElementById("bet").value;
+      
+        if (bet == "" || isNaN(bet)) {
+            alert("Starting bet must be filled in with a number.");
+            document.getElementById("bet").className="error";
+            document.getElementById("bet").focus();
+            return;
+        }
+        
+       
+        bet = parseFloat(parseFloat(document.getElementById("bet").value).toFixed(2));
+      
+      
+        if (bet<=0) {
+          alert("You have to bet some money if you want to play!");
+          document.getElementById("bet").className="error";
+          document.getElementById("bet").focus();
+          return;
+        }
+      
+        play(bet);
+      
+      
+      }
+    
+  function clearErrors() {
+    document.getElementById("bet").className="";
+  }
+  
+  function reset() {
+    document.getElementById("results").style.display="none";
+    document.getElementById("bet").value = "0.00";
+    document.getElementById("play").innerText = "Play";
+  }
